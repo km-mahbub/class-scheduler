648b49a29ccb045c8c1c241b38106b8e5e66fcf2
Mahbub <kmmahbub.zaman@gmail.com>
2020-02-17 13:47:24 +0900

    changed title

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Please Wait. Coming Soon...
         </a>
       </header>
     </div>
